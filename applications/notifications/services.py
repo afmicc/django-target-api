17e9bd493798e527fbc1912f69b8ddfb39c41c9b
Agustin Martinez <agustin.martinez@rootstrap.com>
2020-08-03 12:34:58 -0300

    Notification service

---
+++ applications/notifications/services.py
@@ -0,0 +1,43 @@
+from django.db import transaction
+import environ
+from onesignal_sdk.client import Client as OneSignalClient
+
+from .models import Notification
+
+env = environ.Env()
+
+
+class NotificationSender(object):
+    client = OneSignalClient(app_id=env('ONESIGNAL_APP_ID'), rest_api_key=env('ONESIGNAL_API_KEY'))
+
+    def send_notification(self, emails, data, message):
+        body = self._body(emails, data, message)
+        self.client.send_notification(body)
+
+    def _body(self, emails, data, message):
+        return {
+            'filters': self._create_filter_by_emails(emails),
+            'data': data,
+            'contents': {'en': message},
+        }
+
+    def _create_filter_by_emails(self, emails):
+        email_list = emails if type(emails) is list else [emails]
+
+        def email_filter(email):
+            return {'field': 'tag', 'key': 'email', 'relation': '=', 'value': email}
+
+        def filter_option(i):
+            return email_filter(email_list[int(i/2)]) if i % 2 == 0 else {'operator': 'OR'}
+
+        return [filter_option(i) for i in range(len(email_list*2) - 1)]
+
+
+class NotificationCreator(object):
+    sender = NotificationSender()
+
+    @transaction.atomic
+    def create(self, user, data, title, message):
+        Notification.objects.create(user=user, title=title, message=message)
+
+        self.sender.send_notification(user.email, data, message)
