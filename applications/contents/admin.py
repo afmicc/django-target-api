1ae8e79a40b072f0412886bf57fc1006b9180075
Agustin Martinez <agustin.martinez@rootstrap.com>
2020-08-24 16:26:46 -0300

    Customize admin to display a text area to add the body content

--- applications/contents/admin.py
+++ applications/contents/admin.py
@@ -1,3 +1,21 @@
+from django import forms
 from django.contrib import admin
 
-# Register your models here.
+from .models import Content
+
+
+class ContentModelForm(forms.ModelForm):
+    body = forms.CharField(widget=forms.Textarea)
+
+    class Meta:
+        model = Content
+        fields = '__all__'
+
+
+class ContentAdmin(admin.ModelAdmin):
+    form = ContentModelForm
+    list_display = ('key', 'title', )
+    search_fields = ('key', 'title', )
+
+
+admin.site.register(Content, ContentAdmin)
