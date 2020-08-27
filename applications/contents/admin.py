5e98d776be5c970fd3672a3a26afbe2b060bde61
Agustin Martinez <agustin.martinez@rootstrap.com>
2020-08-27 12:25:30 -0300

    Remove admin form since textfield show textarea by default

--- applications/contents/admin.py
+++ applications/contents/admin.py
@@ -1,19 +1,9 @@
-from django import forms
 from django.contrib import admin
 
 from .models import Content
 
 
-class ContentModelForm(forms.ModelForm):
-    body = forms.CharField(widget=forms.Textarea)
-
-    class Meta:
-        model = Content
-        fields = '__all__'
-
-
 class ContentAdmin(admin.ModelAdmin):
-    form = ContentModelForm
     list_display = ('key', 'title', )
     search_fields = ('key', 'title', )
 
