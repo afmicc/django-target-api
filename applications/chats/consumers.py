38311ed9e55ad5e6595e34e9993565d2ecf192dd
Agustin Martinez <agustin.martinez@rootstrap.com>
2020-08-27 11:48:32 -0300

    Send async notifications when a message is received

--- applications/chats/consumers.py
+++ applications/chats/consumers.py
@@ -1,15 +1,24 @@
+from django.utils.translation import gettext_lazy as _
+
 from channels.db import database_sync_to_async
 from channels.exceptions import DenyConnection
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
+from applications.notifications.services import NotificationCreator
+
 
 class ChatConsumer(AsyncJsonWebsocketConsumer):
+    NOTIFICATION_TITLE = 'New message'
+    NOTIFICATION_BODY = 'New message about {}'
+
+    notificator = NotificationCreator()
+
     async def connect(self):
         self.user = self.scope['user']
         room_id = self.scope['url_route']['kwargs']['room']
 
         if self.user.is_authenticated:
-            self.room = await self._get_room(self.user, room_id)
+            self.room, self.receiver, self.topic = await self._get_room_data(self.user, room_id)
             self.room_group_name = f'chat_room_{room_id}'
 
             if self.room:
@@ -33,6 +42,7 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
             return
 
         await self._save_message(self.user, self.room, message)
+        await self._send_notification_to_receiver(message)
         await self.channel_layer.group_send(
             self.room_group_name,
             {
@@ -49,9 +59,18 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
         await self.send_json(content=data)
 
     @database_sync_to_async
-    def _get_room(self, user, room_id):
-        return user.room_set.get(id=room_id)
+    def _get_room_data(self, user, room_id):
+        room = user.room_set.get(id=room_id)
+        receiver = room.members.exclude(id=user.id).first()
+        return (room, receiver, room.topic)
 
     @database_sync_to_async
     def _save_message(self, user, room, message):
         return room.message_set.create(writer=user, content=message)
+
+    async def _send_notification_to_receiver(self, message):
+        data = {'room_id': self.room.id}
+        title = _(self.NOTIFICATION_TITLE)
+        message = _(self.NOTIFICATION_BODY).format(self.topic.name)
+
+        await self.notificator.create_async(self.receiver, data, title, message)
