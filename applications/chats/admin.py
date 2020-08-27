60b03162d40257146d565c4bc9fe849f8e53d76d
Agustin Martinez <agustin.martinez@rootstrap.com>
2020-08-27 18:01:33 -0300

    Customize Room and Message admin views

--- applications/chats/admin.py
+++ applications/chats/admin.py
@@ -3,5 +3,32 @@ from django.contrib import admin
 from .models import Message, Room
 
 
-admin.site.register(Room)
-admin.site.register(Message)
+class MessagesInline(admin.TabularInline):
+    model = Message
+    fields = ('writer', 'content', 'created_at', 'is_read', )
+    readonly_fields = ('writer', 'content', 'created_at', 'is_read', )
+
+    def has_add_permission(self, *args):
+        return False
+
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('id', 'topic', 'members_names', 'created_at', )
+    fields = ('topic', 'members', 'created_at', )
+    filter_horizontal = ('members', )
+    readonly_fields = ('created_at', )
+    inlines = (MessagesInline, )
+
+    def members_names(self, obj):
+        members = obj.members.all()
+        return f'\'{members.first()}\' and \'{members.last()}\''
+
+    members_names.short_description = 'members'
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'writer', 'content', 'created_at', 'is_read', )
+
+
+admin.site.register(Room, RoomAdmin)
+admin.site.register(Message, MessageAdmin)
